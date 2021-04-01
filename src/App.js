7fca43485c4a01f2bf647d23001acfdc2e7a0ae4
Virginia Gomez Rodriguez <virginiagomezr@gmail.com>
2021-04-01 16:06:11 +0200

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-<<<<<<< HEAD
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hola
-        </p>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-=======
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hola
-        </p>
+        <p>Hola</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
->>>>>>> fa915d7ea07f9d836eda847e5fa09ccc0de6be9d
+export default App
